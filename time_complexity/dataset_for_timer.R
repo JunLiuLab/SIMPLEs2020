7f49f95be52262917dd58d4cbd891587b27f86dc
beyondpie <zsp07@tsinghua.org.cn>
2020-07-21 09:15:45 -0400

    update timer README and comment data source in dataset_for_timer.R

--- time_complexity/dataset_for_timer.R
+++ time_complexity/dataset_for_timer.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 library(here)
 
-
 ## * load original dataset.
+## In our experiment, we use data under
+## TabulaMuris / data / leukocytes_normalized_data.rds
+
 logtpm_gbc <- readRDS(here::here(
   "TabulaMuris", "data",
   "leukocytes_normalized_data.rds"
@@ -12,7 +14,8 @@ cnm <- dim(logtpm_gbc)[2]
 save_sampled_data <- function(mycnm) {
   fnm <- paste0("leu_simple_cell_", mycnm, ".rds")
   logtpm <- logtpm_gbc[, sample(1:cnm, mycnm)]
-  saveRDS(object = logtpm, file = here::here("TabulaMuris", "supplement", "timer", fnm))
+  saveRDS(object = logtpm,
+          file = here::here("TabulaMuris", "supplement", "timer", fnm))
 }
 
 c(1000, seq(2000, 12000, 2000)) %>% map(save_sampled_data)
